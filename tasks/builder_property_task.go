00880a186a98740a652fa27b45fe73f4d94bdd0a
Jose Diaz-Gonzalez <email@josediazgonzalez.com>
2023-01-02 03:35:37 -0500

    feat: add builder_property task

---
+++ tasks/builder_property_task.go
@@ -0,0 +1,37 @@
+package tasks
+
+type BuilderPropertyTask struct {
+	App      string `required:"true" yaml:"app"`
+	Global   bool   `required:"false" yaml:"global"`
+	Property string `required:"true" yaml:"property"`
+	Value    string `required:"false" yaml:"value"`
+	State    string `required:"true" yaml:"state" default:"present"`
+}
+
+func (t BuilderPropertyTask) DesiredState() string {
+	return t.State
+}
+
+func (t BuilderPropertyTask) Execute() TaskOutputState {
+	ctx := PropertyContext{
+		App:      t.App,
+		Global:   t.Global,
+		Property: t.Property,
+		Value:    t.Value,
+	}
+	funcMap := map[string]func() TaskOutputState{
+		"present": func() TaskOutputState {
+			return setProperty("builder:set", ctx)
+		},
+		"absent": func() TaskOutputState {
+			return unsetProperty("builder:set", ctx)
+		},
+	}
+
+	fn := funcMap[t.State]
+	return fn()
+}
+
+func init() {
+	RegisterTask(&BuilderPropertyTask{})
+}
---
+++ tasks/properties.go
@@ -0,0 +1,80 @@
+package tasks
+
+import "fmt"
+
+type PropertyContext struct {
+	App      string `required:"true" yaml:"app"`
+	Global   bool   `required:"false" yaml:"global"`
+	Property string `required:"true" yaml:"property"`
+	Value    string `required:"false" yaml:"value"`
+}
+
+func setProperty(subcommand string, pctx PropertyContext) TaskOutputState {
+	state := TaskOutputState{
+		Changed: false,
+		State:   "absent",
+	}
+
+	if pctx.Global && pctx.App != "" {
+		state.Error = fmt.Errorf("'app' must not be set when 'global' is set to true")
+		return state
+	}
+
+	appName := "--global"
+	if pctx.App != "" {
+		appName = pctx.App
+	}
+
+	if pctx.Value == "" {
+		state.Error = fmt.Errorf("setting a state of 'present' is invalid without a value for 'value'")
+		return state
+	}
+
+	// todo: validate that the value isn't already set
+
+	resp := runDokkuCommand([]string{"--quiet", subcommand, appName, pctx.Property, pctx.Value})
+	if resp.HasError() {
+		state.Error = resp.Error
+		state.Message = string(resp.Stderr)
+		return state
+	}
+
+	state.Changed = true
+	state.State = "present"
+	return state
+}
+
+func unsetProperty(subcommand string, pctx PropertyContext) TaskOutputState {
+	state := TaskOutputState{
+		Changed: false,
+		State:   "present",
+	}
+
+	if pctx.Global && pctx.App != "" {
+		state.Error = fmt.Errorf("'app' must not be set when 'global' is set to true")
+		return state
+	}
+
+	appName := "--global"
+	if pctx.App != "" {
+		appName = pctx.App
+	}
+
+	if pctx.Value != "" {
+		state.Error = fmt.Errorf("setting a state of 'abset' is invalid with a value for 'value'")
+		return state
+	}
+
+	// todo: validate that the value isn't already set
+
+	resp := runDokkuCommand([]string{"--quiet", subcommand, appName, pctx.Property})
+	if resp.HasError() {
+		state.Error = resp.Error
+		state.Message = string(resp.Stderr)
+		return state
+	}
+
+	state.Changed = true
+	state.State = "present"
+	return state
+}
